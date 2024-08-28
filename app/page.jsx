5d6203213d52462564904d8ba415d23c04a57952
Deepak <hdeepak5902@gmail.com>
2024-08-28 23:20:07 +0530

    done with design

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Form = dynamic(() => import("@/components/Homepage/Form"));
 
 const Home = () => {
     return (
-        <section className="flex flex-col items-center justify-center py-24 h-full gap-12 w-full">
+        <section className="flex flex-col items-center justify-center py-12 h-full gap-12 w-full">
             <Title />
             <Form />
         </section>
--- components/PostPage/Form.jsx
+++ components/PostPage/Form.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
 const Form = () => {
     return (
-        <form action="" className="w-full flex flex-col items-center border-2 border-black max-w-[50vw] p-8 gap-12">
+        <form action="" className="w-full flex flex-col items-center bg-gray-100 rounded-sm max-w-[50vw] p-12 gap-12">
             <div className="flex flex-col items-start w-full gap-4">
-                <label htmlFor="prompt">Your AI Prompt</label>
-                <Input id="prompt" placeholder="Share Your Prompts" className="lg:max-w-[50vw] md:max-w-[60vw] sm:max-w-[70vw] ssm:max-w-[80vw] border-gray-600" />
+                <label htmlFor="prompt" className="text-xl font-medium">
+                    Your AI Prompt
+                </label>
+                <Textarea id="prompt" placeholder="Share Your Prompts" className="lg:max-w-[50vw] md:max-w-[60vw] sm:max-w-[70vw] ssm:max-w-[80vw] border-gray-600" />
             </div>
             <div className="flex flex-col items-start w-full gap-4">
-                <label htmlFor="tag">Tag</label>
-                <Input id="tag" className="lg:max-w-[50vw] md:max-w-[60vw] sm:max-w-[70vw] ssm:max-w-[80vw] border-gray-600" />
+                <label htmlFor="tag" className="text-xl font-medium">
+                    Tag
+                </label>
+                <Input id="tag" placeholder="Enter Your Tag" className="lg:max-w-[50vw] md:max-w-[60vw] sm:max-w-[70vw] ssm:max-w-[80vw] border-gray-600" />
             </div>
+            <Button className="text-xl rounded-full">Submit</Button>
         </form>
     );
 };
