58bb52a670231212c2881262fb7ace4bfbb8b747
Deepak <hdeepak5902@gmail.com>
2024-08-29 21:51:27 +0530

    post made responsive

--- components/Homepage/Card.jsx
+++ components/Homepage/Card.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const Card = () => {
     return (
-        <section>
-            <h1>Name</h1>
+        <section className="flex flex-col items-center">
+            <div>
+                <h1>Deepak</h1>
+                <Image src={"/logo.svg"} alt="dummy" width={30} height={30} />
+            </div>
+            <h2>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium numquam dolorem, deleniti vitae laboriosam voluptas unde nulla eius sapiente odit, iure molestiae repellat
+                quae cumque amet consequatur velit distinctio.
+            </h2>
+            <h3>Coding</h3>
         </section>
     );
 };
