4870206a0658a739cdd1008c9a17debf11118cb9
Deepak <hdeepak5902@gmail.com>
2024-09-07 13:16:52 +0530

    rvereted

--- components/Homepage/Post.jsx
+++ components/Homepage/Post.jsx
@@ -6,6 +6,7 @@ import dynamic from "next/dynamic";
 import { SessionContext } from "@/context/SessionProvider";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Protest_Strike } from "next/font/google";
 
 const Card = dynamic(() => import("@/components/Homepage/Card.jsx"));
 
@@ -62,13 +63,13 @@ const Post = ({ query }) => {
         } else {
             setFiltered(posts);
         }
-    }, [query]);
+    }, [query, posts]);
 
     return (
         <>
             {session ? (
                 <section className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 w-full items-center justify-center gap-8 ssm:px-4 sm:px-8">
-                    {posts.map((item, index) => (
+                    {filtered.map((item, index) => (
                         <Card key={index} item={item} handleCopy={() => handleCopy(item.prompt)} />
                     ))}
                 </section>
